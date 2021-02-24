23342acb2d01df07c76db581998420f7ab13428a
Ralf Lang <lang@b1-systems.de>
2021-02-24 14:46:50 +0000

    Development mode for ingo-4.0.0alpha4

--- lib/Application.php
+++ lib/Application.php
@@ -49,7 +49,7 @@ class Ingo_Application extends Horde_Registry_Application
 
     /**
      */
-    public $version = 'H5 (4.0.0alpha2)';
+    public $version = 'H5 (4.0.0alpha3)';
 
     /**
      * Cached list of all rulesets.
